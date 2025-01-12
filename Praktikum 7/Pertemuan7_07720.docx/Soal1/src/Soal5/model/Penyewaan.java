12f9c5dea09574ed9d94c0636835e0cceca39de3
kucingijo0 <adaminsaan24@gmail.com>
2025-01-12 22:00:16 +0700

    Penyewaan.java

--- Praktikum 7/Pertemuan7_07720.docx/Soal1/src/Soal5/model/Penyewaan.java
+++ Praktikum 7/Pertemuan7_07720.docx/Soal1/src/Soal5/model/Penyewaan.java
@@ -1,25 +1,44 @@
 package Soal5.model;
 
-public class Penyewaan {
-    private ItemSewa itemSewa;
-    private int durasiSewa;
+import java.util.ArrayList;
 
-    public Penyewaan(ItemSewa itemSewa, int durasiSewa) {
-        this.itemSewa = itemSewa;
-        this.durasiSewa = durasiSewa;
+public class Penyewaan {
+    private String namaPenyewa;
+    private ArrayList<ItemSewa> daftarItemSewa;
+    private int durasi;
+
+    public Penyewaan(String namaPenyewa, int durasi) {
+        this.namaPenyewa = namaPenyewa;
+        this.durasi = durasi;
+        this.daftarItemSewa = new ArrayList<>();
     }
 
-    public double getTotalHarga() {
-        return itemSewa.hitungTotalHarga(durasiSewa);
+    public void tambahItemSewa(ItemSewa itemSewa) {
+        daftarItemSewa.add(itemSewa);
+    }
+
+    public double hitungTotalHarga() {
+        double totalHarga = 0;
+        for (ItemSewa item : daftarItemSewa) {
+            totalHarga += item.hitungSubtotal(durasi);
+        }
+        return totalHarga;
     }
 
     public String getDetailSewa() {
         StringBuilder detail = new StringBuilder();
-        for (AlatCamping alat : itemSewa.getDaftarAlat()) {
-            detail.append(alat.getNamaAlat())
-                    .append(" (Jenis: ").append(alat.getJenis())
-                    .append(", Harga: ").append(alat.getHargaPerHari()).append(" per hari)\n");
+        detail.append("Nama Penyewa: ").append(namaPenyewa).append("\n")
+                .append("Durasi: ").append(durasi).append(" hari\n")
+                .append("Daftar Alat yang Disewa:\n");
+
+        for (ItemSewa item : daftarItemSewa) {
+            detail.append("- ").append(item.getAlatSewa().getNama())
+                    .append(" (").append(item.getAlatSewa().getJenis()).append(")")
+                    .append(" x ").append(item.getJumlah())
+                    .append(" = Rp ").append(item.hitungSubtotal(durasi)).append("\n");
         }
+
+        detail.append("Total Harga: Rp ").append(hitungTotalHarga()).append("\n");
         return detail.toString();
     }
 }
