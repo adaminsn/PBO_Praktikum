7a8676a82ec419b4f655b32be17b8b1028feb836
kucingijo0 <adaminsaan24@gmail.com>
2025-01-12 22:00:40 +0700

    Controller.java

--- Praktikum 7/Pertemuan7_07720.docx/Soal1/src/Soal5/controller/Controller.java
+++ Praktikum 7/Pertemuan7_07720.docx/Soal1/src/Soal5/controller/Controller.java
@@ -3,25 +3,25 @@ package Soal5.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import Soal5.model.*;
+import Soal5.model.AlatCamping;
+import Soal5.model.ItemSewa;
+import Soal5.model.Penyewaan;
 import Soal5.view.View;
 
 public class Controller {
     private View view;
-    private ItemSewa itemSewa;
     private Penyewaan penyewaan;
 
     public Controller(View view) {
         this.view = view;
-        this.itemSewa = new ItemSewa();
 
         view.tambahListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == view.getBtnTambah()) {
-                    tambahAlatCamping();
+                    tambahItemSewa();
                 } else if (e.getSource() == view.getBtnHitung()) {
-                    hitungHarga();
+                    hitungTotal();
                 } else if (e.getSource() == view.getBtnBatal()) {
                     batal();
                 }
@@ -29,32 +29,36 @@ public class Controller {
         });
     }
 
-    private void tambahAlatCamping() {
+    private void tambahItemSewa() {
+        String namaPenyewa = view.getNamaPenyewa();
+        if (penyewaan == null) {
+            int durasi = view.getDurasi();
+            penyewaan = new Penyewaan(namaPenyewa, durasi);
+        }
+
         String namaAlat = view.getNamaAlat();
         String jenis = view.getJenis();
         double hargaPerHari = view.getHargaPerHari();
+        int jumlah = view.getJumlah();
 
         AlatCamping alatCamping = new AlatCamping(namaAlat, jenis, hargaPerHari);
-        itemSewa.tambahAlat(alatCamping);
-        view.tampilkanPesan("Alat " + namaAlat + " berhasil ditambahkan!");
+        ItemSewa itemSewa = new ItemSewa(alatCamping, jumlah);
+
+        penyewaan.tambahItemSewa(itemSewa);
+        view.tampilkanPesan("Item berhasil ditambahkan: " + namaAlat + " x " + jumlah);
     }
 
-    private void hitungHarga() {
-        if (!itemSewa.getDaftarAlat().isEmpty()) {
-            int durasiSewa = view.getDurasiSewa();
-            penyewaan = new Penyewaan(itemSewa, durasiSewa);
-
+    private void hitungTotal() {
+        if (penyewaan != null) {
             String detailSewa = penyewaan.getDetailSewa();
-            double totalHarga = penyewaan.getTotalHarga();
-
-            view.tampilkanPesan("Detail Sewa:\n" + detailSewa + "\nTotal Harga: Rp " + totalHarga);
+            view.tampilkanPesan("Detail Penyewaan:\n" + detailSewa);
         } else {
-            view.tampilkanPesan("Tambahkan alat camping terlebih dahulu.");
+            view.tampilkanPesan("Belum ada data penyewaan.");
         }
     }
 
     private void batal() {
-        view.tampilkanPesan("Proses dibatalkan.");
-        itemSewa = new ItemSewa();
+        penyewaan = null;
+        view.tampilkanPesan("Proses penyewaan dibatalkan.");
     }
 }
